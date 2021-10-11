9b7bc0c72028bc532ae1edeb57a1033b3500b07a
rasel-031 <raselbishwas997@gmail.com>
2021-10-11 16:27:36 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const data = [
+  {
+    name: "Nautica Men's Bomber Jacket",
+    price: 3000.0,
+    image: "img1.jpg",
+  },
+  {
+    name: "ATG by Wrangler Men's Sherpa Lined Jacket",
+    price: 2000.0,
+    image: "img2.jpg",
+  },
+  {
+    name: "Orvis Men's Classic Barn Coat",
+    price: 2200.0,
+    image: "img3.jpg",
+  },
+  {
+    name: "Wrangler Men's Concealed Carry Blanket Jacket",
+    price: 1000.0,
+    image: "img4.jpg",
+  },
+  {
+    name: "Levi's Men's Varsity Bomber Trucker Jacket",
+    price: 1500.0,
+    image: "img5.jpg",
+  },
+  {
+    name: "Wrangler Men's Western Unlined Denim Jacket",
+    price: 2800.0,
+    image: "img6.jpg",
+  },
+  {
+    name: "Helikon-Tex Pilgrim Anorak Jacket, Bushcraft Line",
+    price: 2900.0,
+    image: "img7.jpg",
+  },
+  {
+    name: "Lee Men's Denim Jacket",
+    price: 1200.0,
+    image: "img8.jpg",
+  },
+  {
+    name: "LW mens Outdoorsman Jacket",
+    price: 900.0,
+    image: "img9.jpg",
+  },
+  {
+    name: "Legendary Whitetails Men's Rugged Full",
+    price: 200.0,
+    image: "img10.jpg",
+  },
+  {
+    name: "Polo Ralph Lauren Men's Core Classic Bi Swing",
+    price: 1600.0,
+    image: "img11.jpg",
+  },
+  {
+    name: "Calvin Klein Men's Denim Trucker Jacket",
+    price: 4000.0,
+    image: "img12.jpg",
+  },
+  {
+    name: "Nautica Men's Lightweight Stretch Golf Jacket",
+    price: 2100.0,
+    image: "img13.jpg",
+  },
+  {
+    name: "Legendary Whitetails Men's Journeyman Shirt Jacket",
+    price: 2700.0,
+    image: "img14.jpg",
+  },
+  {
+    name: "The North Face Men's Apex Bionic 2 Jacket",
+    price: 1100.0,
+    image: "img15.jpg",
+  },
+];
